905873575d32cd3d95684a703d0094f910c2a86c
Awad_lk <muhammad.awad.luckhoo@elca.mu>
2019-11-26 16:27:26 +0400

    feat: candidate-details

--- JobFairApplication/src/app/pages/candidate-details/candidate-details.page.ts
+++ JobFairApplication/src/app/pages/candidate-details/candidate-details.page.ts
@@ -8,11 +8,19 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 })
 export class CandidateDetailsPage implements OnInit {
   myForm: FormGroup;
+  jobs: any;
 
   constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
 
+    this.jobs = [
+      { id: 1, name: 'Software Engineer' },
+      { id: 2, name: 'Front End Developer' },
+      { id: 3, name: 'Javasript Developer' },
+      { id: 4, name: 'Angular Devloper' },
+      { id: 5, name: 'React Developer' },
+      { id: 6, name: 'NodeJS Developer' }
+    ];
   }
-
 }
