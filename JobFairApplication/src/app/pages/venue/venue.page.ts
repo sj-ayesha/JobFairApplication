b5c8952d17e66c8e48bdd264d4af46fcc4a606b5
Ayesha Jownally <sheik.bibi.ayesha.siddiqa.jownally@elca.mu>
2019-12-12 15:40:16 +0400

    fix: venue validation message

--- JobFairApplication/src/app/pages/venue/venue.page.ts
+++ JobFairApplication/src/app/pages/venue/venue.page.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Venue } from 'src/app/model/venue';
 import { ApiService } from 'src/app/services/api.service';
 import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-venue',
@@ -12,6 +14,9 @@ export class VenuePage implements OnInit {
   formVenue: FormGroup;
   venues: Venue[];
   public select ="";
+  submitted = false;
+  venue: Array<string>;
+  
 
   error_messages = {
     venues: [
@@ -19,7 +24,8 @@ export class VenuePage implements OnInit {
     ],
   }
   
-  constructor(private apiService: ApiService, private formBuilder: FormBuilder) {
+  constructor(private apiService: ApiService, private formBuilder: FormBuilder, private router: Router, 
+              private toastCtrl: ToastController) {
     this.formVenue = this.formBuilder.group({
       venues: new FormControl('', Validators.required)
     })
@@ -27,6 +33,12 @@ export class VenuePage implements OnInit {
 
   ngOnInit() {
     this.getVenueByActive();
+
+    this.venue = [
+      'UTM',
+      'UOM'
+    ];
+
   }
 
   selected(id){
@@ -40,5 +52,30 @@ export class VenuePage implements OnInit {
       this.venues = data;
       // console.log(this.venues);
     });
+  }
+
+  onSubmit() {
+    if (this.formVenue.invalid) {
+      console.log(this.submitted, "not sucessful");
+      this.submitted = false;
+      this.UnsuccessMsg();
+    } else {
+      // this.submitCandidate();
+      this.submitted = true;
+      this.router.navigate(['home']);
+
+    }
+
+  }
+
+  async UnsuccessMsg() {
+    const toast = await this.toastCtrl.create({
+      message: 'Please select a venue to proceed',
+      position: 'top',
+      color: 'danger',
+      duration: 2000,
+      cssClass: 'toast-custom'
+    });
+    toast.present();
   }
 }
