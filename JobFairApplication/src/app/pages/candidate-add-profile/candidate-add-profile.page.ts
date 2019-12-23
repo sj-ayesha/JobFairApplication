3e9c39306c947c0d27e7f0c503ffc67497820fd5
Ayesha Jownally <sheik.bibi.ayesha.siddiqa.jownally@elca.mu>
2019-12-23 15:58:19 +0400

    feat: Uploading file working except posting & form validation

--- JobFairApplication/src/app/pages/candidate-add-profile/candidate-add-profile.page.ts
+++ JobFairApplication/src/app/pages/candidate-add-profile/candidate-add-profile.page.ts
@@ -1,17 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators, FormControl, Form, FormArray } from '@angular/forms';
-import { CandidateSkill } from 'src/app/model/CandidateSkill';
-import { ApiService } from 'src/app/services/api.service';
-import { Router, ChildActivationStart } from '@angular/router';
-import { element } from 'protractor';
-import { ToastController } from '@ionic/angular';
-import { Skills } from 'src/app/model/skills';
-import { DropdownsService } from 'src/app/services/dropdowns.service';
+import { Component, OnInit } from "@angular/core";
+import {
+  FormGroup,
+  FormBuilder,
+  Validators,
+  FormControl,
+  Form,
+  FormArray
+} from "@angular/forms";
+import { CandidateSkill } from "src/app/model/CandidateSkill";
+import { ApiService } from "src/app/services/api.service";
+import { Router, ChildActivationStart } from "@angular/router";
+import { element } from "protractor";
+import { ToastController } from "@ionic/angular";
+import { Skills } from "src/app/model/skills";
+import { DropdownsService } from "src/app/services/dropdowns.service";
+import { HttpClient, HttpEventType } from "@angular/common/http";
 
 @Component({
-  selector: 'app-candidate-add-profile',
-  templateUrl: './candidate-add-profile.page.html',
-  styleUrls: ['./candidate-add-profile.page.scss'],
+  selector: "app-candidate-add-profile",
+  templateUrl: "./candidate-add-profile.page.html",
+  styleUrls: ["./candidate-add-profile.page.scss"]
 })
 export class CandidateAddProfilePage implements OnInit {
   formCandidateDetails: FormGroup;
@@ -26,7 +34,7 @@ export class CandidateAddProfilePage implements OnInit {
   CandidateSkills: CandidateSkill[] = [];
   skills: Skills[];
   candidateId: number;
-  selectedDay = '';
+  selectedDay = "";
   public today: any;
   submitted = false;
   position: string;
@@ -44,6 +52,8 @@ export class CandidateAddProfilePage implements OnInit {
   arrayVenue: any[];
   arrayScreening: any[];
 
+  fileData: File = null; // File Upload
+
   // skills: Array<string>;
   // tslint:disable-next-line: variable-name
   // public skills = [
@@ -58,49 +68,41 @@ export class CandidateAddProfilePage implements OnInit {
   // tslint:disable-next-line: variable-name
   error_messages = {
     firstName: [
-      { type: 'required', message: '⚠ First Name is required' },
-      { type: 'maxLength', message: '⚠ First Name must be less than 30 letters' },
-      { type: 'pattern', message: '⚠ First Name is invalid' }
+      { type: "required", message: "⚠ First Name is required" },
+      {
+        type: "maxLength",
+        message: "⚠ First Name must be less than 30 letters"
+      },
+      { type: "pattern", message: "⚠ First Name is invalid" }
     ],
     lastName: [
-      { type: 'required', message: '⚠ Last Name is required' },
-      { type: 'maxLength', message: '⚠ Last Name must be less than 30 letters' },
-      { type: 'pattern', message: '⚠ Last Name is invalid' }
+      { type: "required", message: "⚠ Last Name is required" },
+      {
+        type: "maxLength",
+        message: "⚠ Last Name must be less than 30 letters"
+      },
+      { type: "pattern", message: "⚠ Last Name is invalid" }
     ],
     email: [
-      { type: 'required', message: '⚠ Email is required.' },
-      { type: 'pattern', message: '⚠ Email is invalid.' }
+      { type: "required", message: "⚠ Email is required." },
+      { type: "pattern", message: "⚠ Email is invalid." }
     ],
-    telNumber: [
-      { type: 'pattern', message: '⚠ Telephone number is invalid' }
-    ],
+    telNumber: [{ type: "pattern", message: "⚠ Telephone number is invalid" }],
     mobileNumber: [
-      { type: 'required', message: '⚠ Mobile number is required.' },
-      { type: 'pattern', message: '⚠ Mobile number is invalid' }
+      { type: "required", message: "⚠ Mobile number is required." },
+      { type: "pattern", message: "⚠ Mobile number is invalid" }
     ],
-    nationality: [
-      { type: 'required', message: '⚠ Nationality is required.' },
-    ],
-    gender: [
-      { type: 'required', message: '⚠ Gender is required.' },
-    ],
-    title: [
-      { type: 'required', message: '⚠ Qualification is required.' },
-    ],
-    academyYear: [
-      { type: 'required', message: '⚠ Academy Year is required.' },
-    ],
-    division: [
-      { type: 'required', message: '⚠ Division is required.' },
-    ],
+    nationality: [{ type: "required", message: "⚠ Nationality is required." }],
+    gender: [{ type: "required", message: "⚠ Gender is required." }],
+    title: [{ type: "required", message: "⚠ Qualification is required." }],
+    academyYear: [{ type: "required", message: "⚠ Academy Year is required." }],
+    division: [{ type: "required", message: "⚠ Division is required." }],
     availabilityDate: [
-      { type: 'required', message: '⚠ Availability Date is required.' },
+      { type: "required", message: "⚠ Availability Date is required." }
     ],
-    jobType: [
-      { type: 'required', message: '⚠ Job Type is required.' },
-    ],
+    jobType: [{ type: "required", message: "⚠ Job Type is required." }],
     currentLevel: [
-      { type: 'required', message: '⚠ Current Level is required.' },
+      { type: "required", message: "⚠ Current Level is required." }
     ]
   };
 
@@ -109,57 +111,79 @@ export class CandidateAddProfilePage implements OnInit {
     private apiService: ApiService,
     private router: Router,
     private toastCtrl: ToastController,
-    private dropdowns: DropdownsService
+    private dropdowns: DropdownsService,
+    private http: HttpClient
   ) {
     this.formCandidateDetails = this.formBuilder.group({
-      firstName: new FormControl('', Validators.compose([Validators.maxLength(30), Validators.pattern('[a-zA-Z ]*'), Validators.required])),
-      lastName: new FormControl('', Validators.compose([Validators.maxLength(30), Validators.pattern('[a-zA-Z ]*'), Validators.required])),
-      email: new FormControl('', Validators.compose([
-        Validators.required,
-        Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')
-      ])),
-      mobileNumber: new FormControl('', Validators.compose([
-        Validators.required,
-        Validators.pattern('[0-9]{8}$')
-      ])),
-      telNumber: new FormControl('', Validators.compose([
-        Validators.pattern('[0-9]{7}$')
-      ])),
-      nationality: new FormControl('', Validators.compose([
-        Validators.required,
-        Validators.pattern('^[a-zA-Z]+(([\',. -][a-zA-Z ])?[a-zA-Z]*)*$')
-      ])),
-      gender: new FormControl('', Validators.required),
-      address: new FormControl(''),
-      availabilityDate: new FormControl('', Validators.required),
-      currentAcademicYear: new FormControl(''),
-      jobType: new FormControl('', Validators.required),
+      firstName: new FormControl(
+        "",
+        Validators.compose([
+          Validators.maxLength(30),
+          Validators.pattern("[a-zA-Z ]*"),
+          Validators.required
+        ])
+      ),
+      lastName: new FormControl(
+        "",
+        Validators.compose([
+          Validators.maxLength(30),
+          Validators.pattern("[a-zA-Z ]*"),
+          Validators.required
+        ])
+      ),
+      email: new FormControl(
+        "",
+        Validators.compose([
+          Validators.required,
+          Validators.pattern("^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$")
+        ])
+      ),
+      mobileNumber: new FormControl(
+        "",
+        Validators.compose([
+          Validators.required,
+          Validators.pattern("[0-9]{8}$")
+        ])
+      ),
+      telNumber: new FormControl(
+        "",
+        Validators.compose([Validators.pattern("[0-9]{7}$")])
+      ),
+      nationality: new FormControl(
+        "",
+        Validators.compose([
+          Validators.required,
+          Validators.pattern("^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$")
+        ])
+      ),
+      gender: new FormControl("", Validators.required),
+      address: new FormControl(""),
+      availabilityDate: new FormControl("", Validators.required),
+      currentAcademicYear: new FormControl(""),
+      jobType: new FormControl("", Validators.required),
 
       registrationDate: new FormControl(new Date()),
 
-      currentLevel: new FormControl('', Validators.required),
-      title: new FormControl('', Validators.required),
-      division: new FormControl(''),
-      institution: new FormControl(''),
-      graduationDate: new FormControl(''),
-      candidateId: new FormControl(''),
-      position: new FormControl(''),
-      companyName: new FormControl(''),
-      duration: new FormControl(''),
-      skillId: new FormControl('')
+      currentLevel: new FormControl("", Validators.required),
+      title: new FormControl("", Validators.required),
+      division: new FormControl(""),
+      institution: new FormControl(""),
+      graduationDate: new FormControl(""),
+      candidateId: new FormControl(""),
+      position: new FormControl(""),
+      companyName: new FormControl(""),
+      duration: new FormControl(""),
+      skillId: new FormControl("")
     });
   }
 
- 
-
   ngOnInit() {
-
     // window.localStorage.setItem('priority', JSON.stringify([1, 2, 3]));
     // window.localStorage.setItem('venue_id', JSON.stringify(3));
 
-    const getJobIdLS = window.localStorage.getItem('priority');
+    const getJobIdLS = window.localStorage.getItem("priority");
     const jobId = getJobIdLS[1];
-    window.localStorage.setItem('jobId', jobId);
+    window.localStorage.setItem("jobId", jobId);
 
     this.today = new Date();
     this.day = String(this.today.getDate());
@@ -169,9 +193,8 @@ export class CandidateAddProfilePage implements OnInit {
     this.minutes = this.today.getMinutes();
     this.seconds = this.today.getSeconds();
 
-
     // this.date = (this.year + '-' + this.month + '-' + this.day + 'T' + this.hours + ':' + this.minutes + ':' + this.seconds);
-    this.date = (this.year + '-' + this.month + '-' + this.day);
+    this.date = this.year + "-" + this.month + "-" + this.day;
 
     this.populateSkills();
 
@@ -189,31 +212,36 @@ export class CandidateAddProfilePage implements OnInit {
       return data.checked === true;
     });
 
-    this.arrayExperience = [{
-      companyName: this.formCandidateDetails.get('companyName').value,
-      position: this.formCandidateDetails.get('position').value,
-      duration: this.formCandidateDetails.get('duration').value
-    }];
-    this.arrayQualification = [{
-      title: this.formCandidateDetails.get('title').value,
-      division: this.formCandidateDetails.get('division').value,
-      institution: this.formCandidateDetails.get('institution').value,
-      graduationDate: this.formCandidateDetails.get('graduationDate').value,
-    }];
+    this.arrayExperience = [
+      {
+        companyName: this.formCandidateDetails.get("companyName").value,
+        position: this.formCandidateDetails.get("position").value,
+        duration: this.formCandidateDetails.get("duration").value
+      }
+    ];
+    this.arrayQualification = [
+      {
+        title: this.formCandidateDetails.get("title").value,
+        division: this.formCandidateDetails.get("division").value,
+        institution: this.formCandidateDetails.get("institution").value,
+        graduationDate: this.formCandidateDetails.get("graduationDate").value
+      }
+    ];
     const candidateDetails = {
-      firstName: this.formCandidateDetails.get('firstName').value,
-      lastName: this.formCandidateDetails.get('lastName').value,
-      email: this.formCandidateDetails.get('email').value,
-      telNumber: this.formCandidateDetails.get('telNumber').value,
-      mobileNumber: this.formCandidateDetails.get('mobileNumber').value,
-      gender: this.formCandidateDetails.get('gender').value,
-      address: this.formCandidateDetails.get('address').value,
-      nationality: this.formCandidateDetails.get('nationality').value,
-      registrationDate: this.formCandidateDetails.get('registrationDate').value,
-      availabilityDate: this.formCandidateDetails.get('availabilityDate').value,
-      currentLevel: this.formCandidateDetails.get('currentLevel').value,
-      jobType: this.formCandidateDetails.get('jobType').value,
-      currentAcademicYear: this.formCandidateDetails.get('currentAcademicYear').value,
+      firstName: this.formCandidateDetails.get("firstName").value,
+      lastName: this.formCandidateDetails.get("lastName").value,
+      email: this.formCandidateDetails.get("email").value,
+      telNumber: this.formCandidateDetails.get("telNumber").value,
+      mobileNumber: this.formCandidateDetails.get("mobileNumber").value,
+      gender: this.formCandidateDetails.get("gender").value,
+      address: this.formCandidateDetails.get("address").value,
+      nationality: this.formCandidateDetails.get("nationality").value,
+      registrationDate: this.formCandidateDetails.get("registrationDate").value,
+      availabilityDate: this.formCandidateDetails.get("availabilityDate").value,
+      currentLevel: this.formCandidateDetails.get("currentLevel").value,
+      jobType: this.formCandidateDetails.get("jobType").value,
+      currentAcademicYear: this.formCandidateDetails.get("currentAcademicYear")
+        .value,
       experienceDtos: this.arrayExperience,
       qualificationDtos: this.arrayQualification,
       candidateSkillDtos: filteredCandidateSkills,
@@ -222,26 +250,25 @@ export class CandidateAddProfilePage implements OnInit {
     };
     console.log(candidateDetails);
 
-    this.apiService.saveCandidate(candidateDetails).subscribe(data => {
-      // this.router.navigate(['home']);
-    },
+    this.apiService.saveCandidate(candidateDetails).subscribe(
+      data => {
+        // this.router.navigate(['home']);
+      },
       error => {
         // alert("Data not saved!");
       }
     );
   }
 
-  ionViewWillLoad() {
-
-  }
+  ionViewWillLoad() {}
 
   async successMsg() {
     const toast = await this.toastCtrl.create({
-      message: 'Your information has been succesfully saved',
-      position: 'top',
-      color: 'success',
+      message: "Your information has been succesfully saved",
+      position: "top",
+      color: "success",
       duration: 2000,
-      cssClass: 'toast-custom'
+      cssClass: "toast-custom"
     });
     toast.present();
   }
@@ -262,8 +289,8 @@ export class CandidateAddProfilePage implements OnInit {
       data.forEach((element, index) => {
         let data = {
           skills: element,
-          checked: null,
-        }
+          checked: null
+        };
         if (element !== null && this) {
           this.CandidateSkills.push(data);
         }
@@ -293,7 +320,6 @@ export class CandidateAddProfilePage implements OnInit {
         this.formCandidateDetails.reset();
         this.router.navigate(['home']);
       }, 2000);
-
     }
   }
 
@@ -311,7 +337,8 @@ export class CandidateAddProfilePage implements OnInit {
         <div class="error-messages">
             <ng-container *ngFor='let error of error_messages.position'>
                 <div class="error-message"
-                    *ngIf="addCandidate.get('position').hasError(error.type) && (addCandidate.get('position').dirty || addCandidate.get('position').touched)">
+                    *ngIf="addCandidate.get('position').hasError(error.type) &&
+                     (addCandidate.get('position').dirty || addCandidate.get('position').touched)">
                     {{ error.message }}
                 </div>
             </ng-container>
@@ -324,7 +351,8 @@ export class CandidateAddProfilePage implements OnInit {
         <div class="error-messages">
             <ng-container *ngFor='let error of error_messages.company'>
                 <div class="error-message"
-                    *ngIf="addCandidate.get('company').hasError(error.type) && (addCandidate.get('company').dirty || addCandidate.get('company').touched)">
+                    *ngIf="addCandidate.get('company').hasError(error.type) &&
+                    (addCandidate.get('company').dirty || addCandidate.get('company').touched)">
                     {{ error.message }}
                 </div>
             </ng-container>
@@ -344,7 +372,8 @@ export class CandidateAddProfilePage implements OnInit {
         <div class="error-messages">
             <ng-container *ngFor='let error of error_messages.duration'>
                 <div class="error-message"
-                    *ngIf="addCandidate.get('duration').hasError(error.type) && (addCandidate.get('duration').dirty || addCandidate.get('duration').touched)">
+                    *ngIf="addCandidate.get('duration').hasError(error.type) && 
+                    (addCandidate.get('duration').dirty || addCandidate.get('duration').touched)">
                     {{ error.message }}
                 </div>
             </ng-container>
@@ -356,6 +385,7 @@ export class CandidateAddProfilePage implements OnInit {
     document.getElementById('content').appendChild(div);
   }
 
+  // Tab Button Function
   openTab(event, tabName) {
     let i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName('tabcontent');
@@ -374,7 +404,28 @@ export class CandidateAddProfilePage implements OnInit {
     event.currentTarget.className += ' active';
   }
 
-  onFileSelected(event) {
-    console.log(event);
+  // File Upload Section
+  fileProgress(fileInput: any) {
+    this.fileData = fileInput.target.files[0] as File;
+  }
+
+  onSubmitCV() {
+    const formData = new FormData();
+    formData.append('file', this.fileData);
+    this.http
+      .post('url/to/your/api', formData, {
+        reportProgress: true,
+        observe: 'events'
+      })
+      .subscribe(events => {
+        if (events.type === HttpEventType.UploadProgress) {
+          console.log(
+            'Upload progress: ',
+            Math.round((events.loaded / events.total) * 100) + '%'
+          );
+        } else if (events.type === HttpEventType.Response) {
+          console.log(events);
+        }
+      });
   }
 }
