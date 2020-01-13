95338c67760ece12891d53a6a2d23f13c2084f96
Awad_lk <muhammad.awad.luckhoo@elca.mu>
2020-01-13 18:10:42 +0400

    fix: deployment on android

---
+++ JobFairApplication/src/app/model/jobCategoryDto.ts
@@ -0,0 +1,8 @@
+export class JobCategoryDto {
+    humanResource: number;
+    manager: number;
+    businessAnalyst: number;
+    architect: number;
+    softwareEngineer: number;
+    qualityAssurance: number;
+}
--- JobFairApplication/src/app/pages/home/home.page.ts
+++ JobFairApplication/src/app/pages/home/home.page.ts
@@ -4,6 +4,7 @@ import { Candidate } from 'src/app/model/candidate';
 import { Router } from '@angular/router';
 import { Venue } from 'src/app/model/venue';
 import { CandidateVenueJob } from 'src/app/model/candidateVenueJob';
+import { JobCategoryDto } from 'src/app/model/jobCategoryDto';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -12,6 +13,14 @@ import { CandidateVenueJob } from 'src/app/model/candidateVenueJob';
 export class HomePage implements OnInit{
 
   categoryTitle: [];
+  categoryCount: JobCategoryDto;
+  noCategory = false;
+  softwareEngineer = false;
+  manager = false;
+  humanResource = false;
+  qualityAssurance = false;
+  businessAnalyst = false;
+  architect = false;
 
   candidates: Candidate[];
   venues: Venue[];
@@ -34,6 +43,8 @@ export class HomePage implements OnInit{
     this.populateCandidate();
     this.countCandidatesByVenue();
     console.log('ionViewWillEnter Triggered');
+    this.getCategory();
+  
   }
   ionViewWillLeave(){
     console.log('Left');
@@ -56,6 +67,40 @@ export class HomePage implements OnInit{
         this.noCandidatesAvailable = true;
       } else {
       this.candidateVenueJobs = data;
+      }
+    });
+  }
+
+  private getCategory(): void {
+    this.apiService.getCategoryCount().subscribe(data => {
+      if (data.message === 'JOB_NOT_FOUND') {
+        this.noCategory = true;
+      } else {
+        this.categoryCount = data;
+        if (this.categoryCount.architect == 1){
+          this.architect = true;
+          console.log(this.architect);
+        }
+        if (this.categoryCount.softwareEngineer == 1) {
+          this.softwareEngineer = true;
+          console.log(this.softwareEngineer)
+        }
+        if (this.categoryCount.humanResource == 1){
+          this.humanResource = true;
+          console.log(this.humanResource);
+        }
+        if (this.categoryCount.qualityAssurance == 1) {
+          this.qualityAssurance = true;
+          console.log(this.qualityAssurance)
+        }
+        if (this.categoryCount.businessAnalyst == 1){
+          this.businessAnalyst = true;
+          console.log(this.businessAnalyst);
+        }
+        if (this.categoryCount.manager == 1) {
+          this.manager = true;
+          console.log(this.manager)
+        }
       }
     });
   }
