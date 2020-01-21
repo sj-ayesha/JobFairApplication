f2a4cda6a0a1a1a093df2fb8c1cd04aa1a898ae7
Ayesha Jownally <sheik.bibi.ayesha.siddiqa.jownally@elca.mu>
2020-01-21 14:44:43 +0400

    feat: add dashboard

--- JobFairApplication/src/app/back-office/dashboard/dashboard.page.ts
+++ JobFairApplication/src/app/back-office/dashboard/dashboard.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +8,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardPage implements OnInit {
 
+  @ViewChild('barChart', {static: false}) barChart;
+
+  bars: any;
+  colorArray: any;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ionViewDidEnter() {
+    this.createBarChart();
+  }
+
+  createBarChart() {
+    this.bars = new Chart(this.barChart.nativeElement, {
+      type: 'bar',
+      data: {
+        labels: ['S1', 'S2', 'S3', 'S4', 'S5', 'S6', 'S7', 'S8'],
+        datasets: [{
+          label: 'Viewers in millions',
+          data: [2.5, 3.8, 5, 6.9, 6.9, 7.5, 10, 17],
+          backgroundColor: 'rgb(38, 194, 129)', // array should have same number of elements as number of dataset
+          borderColor: 'rgb(38, 194, 129)',// array should have same number of elements as number of dataset
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true
+            }
+          }]
+        }
+      }
+    });
   }
 
 }
