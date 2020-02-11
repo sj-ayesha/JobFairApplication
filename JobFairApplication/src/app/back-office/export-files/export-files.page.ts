8a36e28b058b03d37b8a26d135497939564daa3a
Ayesha Jownally <sheik.bibi.ayesha.siddiqa.jownally@elca.mu>
2020-02-11 10:03:39 +0400

    feat: export files

---
+++ JobFairApplication/src/app/back-office/export-files/export-files.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-export-files',
+  templateUrl: './export-files.page.html',
+  styleUrls: ['./export-files.page.scss'],
+})
+export class ExportFilesPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
